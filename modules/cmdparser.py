02d533cc62114a5e05f69037c320a21ad003c12b
eldhojv <eldhojv@gmail.com>
2019-11-15 12:03:33 +0530

    correction in metavar

--- modules/cmdparser.py
+++ modules/cmdparser.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Recursively parse folder to display all gzipped files under the directory at any level and to gunzip the files inplace")
-    parser.add_argument("--source_dir", required=False, metavar="source-directory/Images",
+    parser.add_argument("--source_dir", required=False, metavar="source-directory/Data-directory",
                         help="Source directory to parse")
     parser.add_argument("--dryrun", required=True, metavar="true/false", choices=['true','false'],
                         help="True - to display all gzipped files /#/ False - to gunzip all files in place")
